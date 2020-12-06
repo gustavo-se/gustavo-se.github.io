5adf337e40b36f24250d4876bfb0c635c8566fd8
gus <gustavo.segovia03@gmail.com>
2020-12-06 16:57:34 -0300

    comienzo de armado de trending

---
+++ scripts/scriptTrending.js
@@ -0,0 +1,9 @@
+let galeria = document.querySelector('.galeria')
+console.log(galeria)
+// let image = document.createElement('img')
+// galeria.appendChild(image)
+
+// fetch('https://api.giphy.com/v1/gifs/trending?api_key=bdrONB5N1ZSySk8VvFBXF18Yut13R6tX&limit=9&rating=g')
+// .then(res => res.json())
+// .then(res => {image.setAttribute('src', res.data[0].images.original.url)
+//             console.log()})
