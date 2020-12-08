9ba7425c68bd36a53bebc45ac10556f2a6f04866
gus <gustavo.segovia03@gmail.com>
2020-12-08 12:55:54 -0300

    apirest en trending prueba

--- scripts/scriptTrending.js
+++ scripts/scriptTrending.js
@@ -1,9 +1,63 @@
-let galeria = document.querySelector('.galeria')
-console.log(galeria)
-// let image = document.createElement('img')
-// galeria.appendChild(image)
+let gifOfTrendings = document.getElementsByClassName('trending-gif')
+let sliders = document.getElementsByClassName('slider')
+let carrousel = document.querySelector('.carrousel') //xarrousel-slide
 
-// fetch('https://api.giphy.com/v1/gifs/trending?api_key=bdrONB5N1ZSySk8VvFBXF18Yut13R6tX&limit=9&rating=g')
-// .then(res => res.json())
-// .then(res => {image.setAttribute('src', res.data[0].images.original.url)
-//             console.log()})
+window.addEventListener('load',() => {
+    fetch('https://api.giphy.com/v1/gifs/trending?api_key=bdrONB5N1ZSySk8VvFBXF18Yut13R6tX&limit=9&rating=g')
+    .then(res => res.json())
+    .then(res => {
+        for(let i = 1; i< (gifOfTrendings.length - 1) ; i++){
+            gifOfTrendings[i].setAttribute('src', res.data[i].images.original.url)
+        }
+        gifOfTrendings[0].setAttribute('src', res.data[9].images.original.url )
+        gifOfTrendings[10].setAttribute('src', res.data[0].images.original.url )
+    })
+    })
+
+
+let counter = 1
+const size = gifOfTrendings[0].clientWidth
+
+
+carrousel.style.transform = 'translateX(' + (-size * counter) + 'px)'
+
+sliders[1].addEventListener('click', () => {
+    if(counter >= gifOfTrendings.length - 1 ) return
+    carrousel.style.transition = "transform 0.4s ease-in-out"
+    counter++
+    carrousel.style.transform = 'translateX(' + (-size * counter) + 'px)'
+})
+
+sliders[0].addEventListener('click', () => {
+    if(counter<=0) return
+    carrousel.style.transition = "transform 0.4s ease-in-out"
+    counter--
+    carrousel.style.transform = 'translateX(' + (-size * counter) + 'px)'
+})
+
+carrousel.addEventListener('transitionend', ()=>{
+    if(gifOfTrendings[counter].id === 'last-clone'){
+        carrousel.style.transition = "none"
+        counter = gifOfTrendings.length -2 
+        carrousel.style.transform = 'translateX(' + (-size * counter) + 'px)'
+    }
+    if(gifOfTrendings[counter].id === 'first-clone'){
+        carrousel.style.transition = "none"
+        counter = gifOfTrendings.length - counter
+        carrousel.style.transform = 'translateX(' + (-size * counter) + 'px)'
+    }
+})
+
+sliders[0].addEventListener('mouseover',() => {
+     sliders[0].setAttribute('src', './img/button-slider-left-hover.svg')})
+ sliders[0].addEventListener('mouseout', () => {
+     sliders[0].setAttribute('src', './img/button-slider-left.svg')
+ })
+
+ sliders[1].addEventListener('mouseover',() => {
+     sliders[1].setAttribute('src', './img/Button-Slider-right-hover.svg')})
+ sliders[1].addEventListener('mouseout', () => {
+     sliders[1].setAttribute('src', './img/Button-Slider-right.svg')
+ })
+
+ 
