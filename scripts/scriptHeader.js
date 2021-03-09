0bfd1127727adbf4c4da4b82d05c2b5398acc479
Gustavo <68789743+gustavo-se@users.noreply.github.com>
2021-03-09 17:27:09 -0300

    Agregando estilos en mobile (falta compilar)

--- scripts/scriptHeader.js
+++ scripts/scriptHeader.js
@@ -1,14 +1,11 @@
-menu.addEventListener('click', ()=> {
-    ulDesplegado.classList.toggle('ul-desplegado');
-    close.classList.toggle('burger-close');
-    hamburguesa.classList.toggle('close-burger')
-} );
-close.addEventListener('click', () => {
-    ulDesplegado.classList.toggle('ul-desplegado');
-    close.classList.toggle('burger-close');
-    hamburguesa.classList.toggle('close-burger');
-})
-
-
-
-
+menu.addEventListener("click", () => {
+  ulDesplegado.classList.toggle("ul-desplegado");
+  close.classList.toggle("burger-close");
+  hamburguesa.classList.toggle("close-burger");
+});
+close.addEventListener("click", () => {
+  ulDesplegado.classList.toggle("ul-desplegado");
+  close.classList.toggle("burger-close");
+  hamburguesa.classList.toggle("close-burger");
+});
+console.log(hola);
