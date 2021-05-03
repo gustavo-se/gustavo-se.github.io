a78b9fc28aa8739479381de58f50775c5b5f4ba4
Gustavo Segovia <gustavo.segovia03gmail.com>
2021-05-03 18:45:31 -0300

    tipado scriptOtherFunctions

--- scripts/scriptOtherFunctions2.js
+++ scripts/scriptOtherFunctions2.js
@@ -1,50 +1,50 @@
-"use strict";
 //Eventos en botones del gif maximizado
+import { btnHover, download, maxGifClose, principalPage, } from './scriptHoversAndFunctions2';
 //Fav button
-maxButtomFav.addEventListener("click", () => {
-    if (maxButtomFav.getAttribute("src") === favButton) {
-        maxButtomFav.setAttribute("src", favActiveButton);
-        maxButtomFav.classList.add("fav-icon-active");
+maxButtomFav.addEventListener('click', () => {
+    if (maxButtomFav.getAttribute('src') === favButton) {
+        maxButtomFav.setAttribute('src', favActiveButton);
+        maxButtomFav.classList.add('fav-icon-active');
     }
     else {
-        maxButtomFav.setAttribute("src", favButton);
-        maxButtomFav.classList.remove("fav-icon-active");
+        maxButtomFav.setAttribute('src', favButton);
+        maxButtomFav.classList.remove('fav-icon-active');
     }
 });
 //Download button
-maxButtomDownload.addEventListener("click", () => {
+maxButtomDownload.addEventListener('click', () => {
     download(gifMax.firstElementChild);
 });
 //Close button
-closeExpand.addEventListener("click", () => {
+closeExpand.addEventListener('click', () => {
     maxGifClose();
 });
 //Pagina principal al hacer click al logo gifos
-logoGifos.addEventListener("click", () => {
+logoGifos.addEventListener('click', () => {
     principalPage();
 });
 //Hovers de Redes Sociales
-footer.addEventListener("mouseover", (e) => {
-    if (footer.classList.contains("fondo-noc-first")) {
-        btnHover(e, "facebook", "./img/icon_facebook_noc.svg");
-        btnHover(e, "twitter", "./img/icon_twitter_noc.svg");
-        btnHover(e, "instagram", "./img/icon_instagram_noc.svg");
+footer.addEventListener('mouseover', e => {
+    if (footer.classList.contains('fondo-noc-first')) {
+        btnHover(e, 'facebook', './img/icon_facebook_noc.svg');
+        btnHover(e, 'twitter', './img/icon_twitter_noc.svg');
+        btnHover(e, 'instagram', './img/icon_instagram_noc.svg');
     }
     else {
-        btnHover(e, "facebook", "./img/icon_facebook_hover.svg");
-        btnHover(e, "twitter", "./img/icon-twitter-hover.svg");
-        btnHover(e, "instagram", "./img/icon_instagram-hover.svg");
+        btnHover(e, 'facebook', './img/icon_facebook_hover.svg');
+        btnHover(e, 'twitter', './img/icon-twitter-hover.svg');
+        btnHover(e, 'instagram', './img/icon_instagram-hover.svg');
     }
 });
-footer.addEventListener("mouseout", (e) => {
-    if (footer.classList.contains("fondo-noc-first")) {
-        btnHover(e, "facebook", "./img/icon_facebook.svg");
-        btnHover(e, "twitter", "./img/icon-tw-normal.svg");
-        btnHover(e, "instagram", "./img/icon_instagram.svg");
+footer.addEventListener('mouseout', e => {
+    if (footer.classList.contains('fondo-noc-first')) {
+        btnHover(e, 'facebook', './img/icon_facebook.svg');
+        btnHover(e, 'twitter', './img/icon-tw-normal.svg');
+        btnHover(e, 'instagram', './img/icon_instagram.svg');
     }
     else {
-        btnHover(e, "facebook", "./img/icon_facebook.svg");
-        btnHover(e, "twitter", "./img/icon-twitter.svg");
-        btnHover(e, "instagram", "./img/icon_instagram.svg");
+        btnHover(e, 'facebook', './img/icon_facebook.svg');
+        btnHover(e, 'twitter', './img/icon-twitter.svg');
+        btnHover(e, 'instagram', './img/icon_instagram.svg');
     }
 });
